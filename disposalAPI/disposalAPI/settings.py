37177454f14b95d07c9425f6bd4f22674eec1108
Kacper <k.wojciak0036@gmail.com>
2023-01-24 00:05:41 +0100

    feat: add swagger

--- disposalAPI/disposalAPI/settings.py
+++ disposalAPI/disposalAPI/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'drf_yasg',
     'offer.apps.OfferConfig',
     'clients.apps.ClientsConfig',
     'orders.apps.OrdersConfig',
--- disposalAPI/disposalAPI/urls.py
+++ disposalAPI/disposalAPI/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 from offer.views import ContainerViewSet, PriceViewSet
 from clients.views import ClientViewSet, AddressViewSet
 from orders.views import OrderViewSet, OrderListViewSet
@@ -28,9 +32,22 @@ router.register(r'addresses', AddressViewSet)
 router.register(r'Containers', ContainerViewSet)
 router.register(r'prices', PriceViewSet)
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Disposal API",
+      default_version='v1',
+      description="API for disposal container distributors",
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
 
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
+    path('swagger.json/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
